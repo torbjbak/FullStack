cc63c53158fc420ff77705a89375a9e534f7f194
torbjbak <tbakke93@gmail.com>
2021-03-12 12:39:09 +0100

    Initial commit

---
+++ db/src/test/java/oving3/db/DbApplicationTests.java
@@ -0,0 +1,13 @@
+package oving3.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
