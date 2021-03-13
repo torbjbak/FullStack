0712c5a9129345e0a4f631131187932bca9d624b
torbjbak <tbakke93@gmail.com>
2021-03-13 20:15:54 +0100

    integrating vue functionality (ish)

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
 const app = Vue.createApp({
     data() {
         return {
-            result: 'Number'
+            result: '',
+            writing: String[3],
+            currentNumber:  String[10],
+            counter: 0,
+            digit: 0,
+            calculations: ['calc 1', 'calc 2', 'calc 3']
+        }
+    },
+    
+    methods: {
+        addNumber(number) {
+            currentNumber[digit] = number;
+            digit++;
+            if(counter%2 == 1) {
+                counter++;
+            }
+        },
+        addOperator(operator) {
+            digit = 0;
+            writing[1] = operator;
+            if(counter == 0) {
+                writing[0] == currentNumber.join('');
+                currentNumber = [];
+                counter++;
+            }
+        },
+        clickEquasls() {
+            if(counter == 2) {
+                result = '7';
+            }
         }
     }
-})
+})  
