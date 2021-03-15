6301f6395a6f58096c6212e1bc44f05ff72a0abf
torbjbak <tbakke93@gmail.com>
2021-03-15 14:13:45 +0100

    MADE CALCULATOR

--- main.js
+++ main.js
@@ -1,35 +1,102 @@
 const app = Vue.createApp({
     data() {
         return {
-            result: '',
-            writing: String[3],
-            currentNumber:  String[10],
-            counter: 0,
-            digit: 0,
-            calculations: ['calc 1', 'calc 2', 'calc 3']
+            answer:             0,
+            result:             '',
+            writing:            '',
+            writeArray:         ['', '', ''],
+            argCounter:         0,
+            hasPoint:           false,
+            results:            ['', '', '', '', '', '', '', '', '', '']
         }
     },
     
     methods: {
-        addNumber(number) {
-            currentNumber[digit] = number;
-            digit++;
-            if(counter%2 == 1) {
-                counter++;
+        digit(number) {
+            if(this.argCounter%2 == 1) {
+                this.argCounter++;
+            }
+            this.writeArray[this.argCounter] += number;
+            this.writing = this.writeArray.join(' ');
+        },
+        operator(operator) {
+            if(this.argCounter%2 == 0) {
+                if(this.writeArray[this.argCounter] == '') {
+                    this.writeArray[this.argCounter] += '0';
+                }
+                this.argCounter++;
+                this.hasPoint = false;
+            }
+            this.writeArray[this.argCounter] = operator;
+            this.writing = this.writeArray.join(' ');
+        },
+        pi() {
+            if(this.argCounter%2 == 1) {
+                this.argCounter++;
+            }
+            this.writeArray[this.argCounter] = '3.14159';
+            this.hasPoint = true;
+            this.writing = this.writeArray.join(' ');
+        },
+        ans() {
+            if(this.argCounter%2 == 1) {
+                this.argCounter++;
+            }
+            this.writeArray[this.argCounter] = this.result;
+            this.hasPoint = true;
+            this.writing = this.writeArray.join(' ');
+        },
+        point() {
+            if(!this.hasPoint) {
+                if(this.argCounter%2 == 0) {
+                    if(this.writeArray[this.argCounter] == '') {
+                        this.writeArray[this.argCounter] += '0';
+                    }
+                    this.writeArray[this.argCounter] += ('.'); 
+                } else {
+                    this.argCounter++;
+                    this.writeArray[this.argCounter] = ('0.'); 
+                }
+                this.hasPoint = true;
+                this.writing = this.writeArray.join(' ');
             }
         },
-        addOperator(operator) {
-            digit = 0;
-            writing[1] = operator;
-            if(counter == 0) {
-                writing[0] == currentNumber.join('');
-                currentNumber = [];
-                counter++;
-            }
+        del() {
+            this.writeArray[this.argCounter] = '';
+            this.writing = this.writeArray.join(' ');
         },
-        clickEquasls() {
-            if(counter == 2) {
-                result = '7';
+        clear() {
+            this.writeArray = ['', '', '']
+            this.writing = '';
+            this.argCounter = 0;
+        },
+        equals() {
+            if(this.argCounter < 2) {
+                this.result = this.writeArray[0];
+            } else {
+                this.result = this.calculate(this.writeArray);
+            }
+            for(var i = 9; i > 0; i--) {
+                this.results[i] = this.results[i-1]
+            }
+            this.results[0] = this.writing +' = '+ this.result;
+            this.clear();
+        },
+        calculate(args) {
+            let arg1 = parseFloat(args[0]);
+            let arg2 = parseFloat(args[2]);
+            let op = args [1];
+            switch(op) {
+                case '+':
+                    return (arg1 + arg2).toFixed(2).toString();
+                case '-':
+                    return (arg1 - arg2).toFixed(2).toString();
+                case '*':
+                    return (arg1 * arg2).toFixed(2).toString();
+                case '/':
+                    return (arg1 / arg2).toFixed(2).toString();
+                default:
+                    return 'Error?';
             }
         }
     }
