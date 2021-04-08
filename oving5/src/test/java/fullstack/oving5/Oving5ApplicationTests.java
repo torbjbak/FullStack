e9db6a190c05d52d8f870ba9fcf9ccfe72544c92
torbjbak <tbakke93@gmail.com>
2021-04-08 14:37:41 +0200

    html/css

---
+++ oving5/src/test/java/fullstack/oving5/Oving5ApplicationTests.java
@@ -0,0 +1,13 @@
+package fullstack.oving5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oving5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
